c38efe5922292628563bc09164fae23cb0eb379b
philonggroup <bookingcardiy@gmail.com>
2026-05-24 07:53:29 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: { extend: { colors: { primary: '#F59E0B', dark: '#0F172A' } } },
+  plugins: [],
+};
